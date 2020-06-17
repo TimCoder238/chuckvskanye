83894b03ac543f1c8b6704d283835ba46f6cc0ef
Tim Costa <costa.tim@live.be>
2020-06-17 12:31:06 +0200

    Updates

---
+++ precache-manifest.cea9f03291ce1bb340af6c12e62873b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5590b98f78e9ab28d2494d2a45d4d11e",
+    "url": "/chuckvskanye/index.html"
+  },
+  {
+    "revision": "89b7caffc501e3097a1b",
+    "url": "/chuckvskanye/static/css/main.e181f0e9.chunk.css"
+  },
+  {
+    "revision": "6795043894aa98e9e00e",
+    "url": "/chuckvskanye/static/js/2.4bb37bfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chuckvskanye/static/js/2.4bb37bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89b7caffc501e3097a1b",
+    "url": "/chuckvskanye/static/js/main.fb7b5388.chunk.js"
+  },
+  {
+    "revision": "2626b7964b175c18f0c4",
+    "url": "/chuckvskanye/static/js/runtime-main.5193c6ff.js"
+  },
+  {
+    "revision": "80768de0d532b89617294edbae3fca70",
+    "url": "/chuckvskanye/static/media/chuck.80768de0.gif"
+  },
+  {
+    "revision": "bb90b8a740b6fba66036e592f11149da",
+    "url": "/chuckvskanye/static/media/kanye2.bb90b8a7.png"
+  }
+]);
